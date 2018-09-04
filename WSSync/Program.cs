92394b7dc3f3a7b2186cab15a8509afa8616e647
Rudolf Zanoni <rudolf@devplace.com.br>
2018-09-04 13:58:05 -0300

    [TEC] Robo de acionamento da sincronização;

---
+++ WSSync/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ServiceProcess;
+
+namespace WSSync
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+
+            ServicesToRun = new ServiceBase[]
+            {
+                new Sync()
+            };
+
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ WSSync/Sync.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.ServiceProcess;
+using System.Timers;
+using System.Web.Configuration;
+
+namespace WSSync
+{
+    public partial class Sync : ServiceBase
+    {
+        Timer timer = new Timer();
+
+        #region [ DI ]
+
+        public Sync()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        private void RunSync()
+        {
+            string url = WebConfigurationManager.AppSettings["UrlBase"];
+            string[] urls = WebConfigurationManager.AppSettings["RunUrls"].Split('|');
+            System.Threading.Tasks.Task objTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                foreach (var item in urls)
+                {
+                    using (HttpClient httpClient = new HttpClient())
+                    {
+                        string URL = string.Format("{0}/{1}", url, item);
+
+                        var response = httpClient.GetAsync(URL).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            EventLog.WriteEntry(string.Format("Chamada da URL {0} data {1}, executada com sucesso.", item, DateTime.Now.ToShortTimeString()), EventLogEntryType.Information);
+                        }
+                        else
+                        {
+                            EventLog.WriteEntry(string.Format("Chamada da URL {0} data {1}, executada com falha. Erro: {2}", item, DateTime.Now.ToShortTimeString(), response.RequestMessage), EventLogEntryType.Information);                            
+                        }
+                    }
+                }
+            });
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            EventLog.WriteEntry("Executando chamada das URLs: " +
+                DateTime.Now.ToShortTimeString(), EventLogEntryType.Information);
+
+            RunSync();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            EventLog.WriteEntry("Serviço Inicializado.", EventLogEntryType.Information);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 60000;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            timer.Enabled = false;
+            EventLog.WriteEntry("Serviço Parado.", EventLogEntryType.Information);
+        }
+    }
+}
