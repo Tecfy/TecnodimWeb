17007b1660195218c15e4becbc1f26c3d4dee8d2
Rudolf Zanoni <rudolf.zanoni@devplace.com.br>
2019-10-17 17:39:20 -0300

    [TEC] Criação de login e perfil básico para TecnodimWeb + Cappservice a partir do AD;

---
+++ Site/Controllers/Api/ImportUsersController.cs
@@ -0,0 +1,43 @@
+﻿using Model.In;
+using Repository;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Site.Api.Controllers
+{
+    [RoutePrefix("Api/ImportUsers")]
+    public class ImportUsersController : ApiController
+    {
+        private RegisterEventRepository registerEventRepository = new RegisterEventRepository();
+        private ImportUserRepository importUserRepository = new ImportUserRepository();
+
+        [AllowAnonymous, HttpGet]
+        public HttpResponseMessage GetImportUsers()
+        {
+            var currentContext = HttpContext.Current;
+
+            System.Threading.Tasks.Task objTask = System.Threading.Tasks.Task.Factory.StartNew(() =>
+            {
+                HttpContext.Current = currentContext;
+
+                string Key = Guid.NewGuid().ToString();
+
+                try
+                {
+                    ImportUsersIn importUsersIn = new ImportUsersIn() { key = Key };
+
+                    importUserRepository.GetImportUsers(importUsersIn);
+                }
+                catch (Exception ex)
+                {
+                    registerEventRepository.SaveRegisterEvent("", Key, "Erro", "Tecnodim.Controllers.ImportUsersController.GetImportUsers", ex.Message);
+                }
+            });
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
