2b689c8d48d1e659546623a9a9970c58a514c416
Rudolf Zanoni <rudolf.zanoni@devplace.com.br>
2019-11-11 14:30:58 -0300

    [TEC] - API - Reenvio de Dossiê - Busca de Documento;

--- Model/ResendDocument/VM/ResendDocumentsVM.cs
+++ Model/ResendDocument/VM/ResendDocumentsVM.cs
@@ -4,11 +4,15 @@ namespace Model.VM
 {
     public class ResendDocumentsVM
     {
+        public string id { get; set; }
+
         public string registration { get; set; }
 
         public string cpf { get; set; }
 
         public string name { get; set; }
+
+        public string title { get; set; }
 
         public List<ResendDocumentItemVM> itens { get; set; }
     }
